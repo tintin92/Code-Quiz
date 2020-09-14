aad5e318232e298f0c496559876a7b698358f4cd
Tintin <tanner@schenck.xyz>
2020-09-14 19:51:15 -0400

    created index.html file w pseudo code to begin coding out quiz, wrote out questions in question.js file

---
+++ assets/js/questions.js
@@ -0,0 +1,13 @@
+var questions = [
+    {
+        title: "Example Question 1:", 
+        choices: ["Choices 1", "Choice 2", "Choice 3", "Choice 4"],
+        answer: "answer from choices"
+    },
+    {
+        title: "Example Question 2:", 
+        choices: ["Choices 1", "Choice 2", "Choice 3", "Choice 4"],
+        answer: "answer from choices"
+
+    }
+]; 
