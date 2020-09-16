1164b28c6fdeded712587871aebd04a55a1cafc7
Tintin <tanner@schenck.xyz>
2020-09-16 19:10:11 -0400

    update js file with questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,13 +1,52 @@
 var questions = [
     {
-        title: "Example Question 1:", 
-        choices: ["Choices 1", "Choice 2", "Choice 3", "Choice 4"],
-        answer: "answer from choices"
+        title: "Question 1: The _______ API allows you to manipulate HTML and CSS?", 
+        choices: ["google", "tiger", "DOM (Document Object Model)", "md"],
+        answer: "DOM (Document Object Model)"
+    },
+    {   title: "Question 2: Inside which HTML element do we put the JavaScript?", 
+        choices: ["<js>", "scripting", "<java>", "<script>"],
+        answer: "<script>"
     },
     {
-        title: "Example Question 2:", 
-        choices: ["Choices 1", "Choice 2", "Choice 3", "Choice 4"],
-        answer: "answer from choices"
-
+        title: "Question 3: What is the JavaScript filename extension?", 
+        choices: [".md", ".com", ".pdf", ".js"],
+        answer: ".js"
+    },
+    {     
+        title: "Question 4:Choose the correct HTML element for the largest heading...", 
+        choices: ["<head>", "<heading>", "<h6>", "<h1>"],
+        answer: "<h1>"
+    },
+    {    
+  
+        title: "Question 5: What is the correct HTML element for inserting a line break?", 
+        choices: ["<rb>", "<br>", "break", "lb"],
+        answer: "<br>"
     }
-]; 
+];
+//     }     
+  
+//         title: "Question 6: ", 
+//         choices: ["", "", "", ""],
+//         answer: ""
+
+//   } 
+// }     
+  
+//         title: "Question 7: ", 
+//         choices: ["", "", "", ""],
+//         answer: ""
+
+//   } 
+// }     
+  
+//         title: "Question 5: ", 
+//         choices: ["", "", "", ""],
+//         answer: ""
+
+//   } 
+    
+    
+    
+    // ];
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
-var questionIndex = 0; 
-var clockShot = questions.length * 10
+
+// var questionsIndex = 0;
+// var time = questions.length * 10;
+// var timer;
 
 
 var displayQuestionEl = document.querySelector(".display-questions");
@@ -9,10 +11,13 @@ var mainDisplay = document.createElement("h3");
 var startBtn = document.createElement("button");
 
 
-var timer = 5;
 
-
+//global variables
+var timer = 60;
+// variable to store current index
+var index = 0;
 //****Functions**** */
+
 
 function openingPage() {
     mainDisplay.textContent = "Press the button to start"
@@ -28,54 +33,52 @@ function startQuiz() {
     nextQuestion()
 }
 
-// funtion the handles timer 
+// Reduce clock by 1 second off of clock
 function showTimer() {
     // diplay timer to screen
-    timerCount.textContent = timer; 
+    timerCount.textContent = timer;
     // create setInterval and store it to a variable
-    var questionTimer = setInterval(function () {
-        // decrease timer by 1
-        timer--
+    var questionTimer = setInterval(function() {
+        // countdown by 1 
+        timer--;
         //display timer to screen
-        timerCount.textContent = timer; 
+        timerCount.textContent = timer;
         // if timer goes down to 0, clear interval
-        if ( timer <= 0) {
+        if (timer <= 0) {
             clearInterval(questionTimer);
         }
-    }, 1000)
+    }, 1*1000)
 
 }
-   
-// // function that handles and displays the next question
-function nextQuestion() {
-// declare a variable to store current question. Assign the current question.
-    // Locates current question object from array 
-    var currentQuestion = questions[questionIndex];
-//console.log current question
-    console.log(currentQuestion);
 
+// function that handles and displays the next question
+function nextQuestion() {
+    // Show a question from the array
+    var currentQuestion = questions[index];
+    //console.log current question
+    console.log(currentQuestion);
+    //Display question after clicking start
     displayQuestionEl.textContent = "";
 
     mainDisplay.textContent = currentQuestion.title;
 
     displayQuestionEl.append(mainDisplay);
-    
-    var choicesContainer = document.createElement("div");
 
+var choicesContainer = document.createElement("div");
+    //Loops through multiple choice questions
     for (let questionIndex = 0; questionIndex < currentQuestion.choices.length; questionIndex++) {
+        //Each button has a choice we will be able to track
         var choiceBtn = document.createElement("button");
 
         choiceBtn.textContent = currentQuestion.choices[questionIndex];
-
+        //Checks answer by making a button 
         choiceBtn.addEventListener("click", checkAnswer);
+        // Append to display 
+        choicesContainer.append(choiceBtn);
 
-        choicesContainer.append(choiceBtn); 
-        
     }
+    //Must append to show question choices on main display
     displayQuestionEl.append(choicesContainer);
-
-   
-
 }
 
 //*****Logic to check for right answer */
@@ -84,12 +87,12 @@ function checkAnswer(event) {
 
     //***Logic to CHECK FOR RIGHT ANSWER */
     var responseText = event.target.textContent;
-    console.log(responseText); 
+    console.log(responseText);
 
 
     //increase index by 1 
-    index ++
-    if (responseText === questions[index].answer){
+    index++
+    if (responseText == questions[index].answer) {
         console.log("Correct");
     } else {
         console.log("Incorrect");
@@ -99,7 +102,7 @@ function checkAnswer(event) {
 
 }
 
-// // // add event listener to start quiz 
+// add event listener to start quiz 
 startBtn.addEventListener("click", startQuiz)
 // call function to show opening page
 openingPage(); 
