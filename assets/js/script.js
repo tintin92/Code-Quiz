502ecf9d2b46885b2e17771647a4654573bfcb96
Tintin <tanner@schenck.xyz>
2020-09-16 02:17:43 -0400

    Correctly displaying start button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,112 +1,105 @@
+var questionIndex = 0; 
+var clockShot = questions.length * 10
 
 
-// hook elements from the page (ex. var exampleElement = document.querySelector(".elementClass"))
-// // // Hook question container element
 var displayQuestionEl = document.querySelector(".display-questions");
-// Hook timer element 
 var timerCount = document.querySelector(".timer");
 var resultsEl = document.querySelector(".results");
-
-//Create dynamic elements (i.e. exampleDynamicE1 = document.createElement("button"))
-// - create h3 to Show Instructions Text and Questions
 var mainDisplay = document.createElement("h3");
-// // // - create Button to Start Quiz
 var startBtn = document.createElement("button");
 
-// // // Declare global variables
-// - variable to store timer number 
-var timer = 60;
-// - variable to store current index
-var index = 0;
+
+var timer = 5;
+
 
 //****Functions**** */
 
 function openingPage() {
-//add text to h3 element
     mainDisplay.textContent = "Press the button to start"
-    // add text to Button
     startBtn.textContent = "Start"
-    // append text and button to question container
     displayQuestionEl.append(mainDisplay, startBtn)
 }
 
+// create a function to start game 
+function startQuiz() {
+    // show timer function
+    showTimer()
+    // call next question function
+    nextQuestion()
+}
 
-// function that shows the question and starts the timer
+// funtion the handles timer 
+function showTimer() {
+    // diplay timer to screen
+    timerCount.textContent = timer; 
+    // create setInterval and store it to a variable
+    var questionTimer = setInterval(function () {
+        // decrease timer by 1
+        timer--
+        //display timer to screen
+        timerCount.textContent = timer; 
+        // if timer goes down to 0, clear interval
+        if ( timer <= 0) {
+            clearInterval(questionTimer);
+        }
+    }, 1000)
 
-// // function startQuiz() {
-// //     // show timer function
-// //     // call next question function
+}
+   
+// // function that handles and displays the next question
+function nextQuestion() {
+// declare a variable to store current question. Assign the current question.
+    // Locates current question object from array 
+    var currentQuestion = questions[questionIndex];
+//console.log current question
+    console.log(currentQuestion);
 
-// // }
+    displayQuestionEl.textContent = "";
 
-// // // funtion the handles timer 
-// // function showTimer() {
-// //         // diplay timer to screen
-// //         timerCount.textContent = timer;
+    mainDisplay.textContent = currentQuestion.title;
 
-// //         // create setInterval and store it to a variable
-// //         var questionTimer = setInterval(function () {
-// //         // inside set Interval function:
-// //         // decrease timer by 1 
-// //         timer-- 
-// //         //display timer to screen
-// //         timerCount.textContent = timer;
-// //         // if timer goes down to 0, clear interval
-// //             if (timer <= 0) {
-// //                 clearInterval(questionTimer);
-// //             }
+    displayQuestionEl.append(mainDisplay);
+    
+    var choicesContainer = document.createElement("div");
 
-// //     }, 1 * 1000)
+    for (let questionIndex = 0; questionIndex < currentQuestion.choices.length; questionIndex++) {
+        var choiceBtn = document.createElement("button");
 
-// // }
-// // // function that handles and displays the next question
-// // function nextQuestion() {
-// //     // declare a variable to store current question. Assign the current question. 
-// //     var currentQuestion = questions [index];
-// //     //console.log current question
-// //     console.log(currentQuestion);
-// //     //emtpy question container element;
-// //     displayQuestionEl.textContent = ""; 
-// //     // add current question title to the main text display variable
-// //     mainDisplay.textContent = currentQuestion.title;
-// //     // append the main text display variable to the question container element
-// //     displayQuestionEl.append(mainDisplay);
-// //     // create a div element to wrap the "choices"
-// //     var choicesContainer = document.createElement("button");
-// //     // use a loop to: 
-// //     for (let i=0; i < currentQuestion.choices.length; i++) {
+        choiceBtn.textContent = currentQuestion.choices[questionIndex];
+
+        choiceBtn.addEventListener("click", checkAnswer);
+
+        choicesContainer.append(choiceBtn); 
         
-// //         //     - create button elements for each choice
-// //         var choiceBtn = document.createElement("button");
-// //         //     - add text to each button from question choices
-// //         choiceBtn.textContent = currentQuestion.choices[i];
-// //         //     - add a click event listener to button to check answers 
-// //         choiceBtn.addEventListener("click" , checkAnswer);
-// //         //     - append buttons to div element created to wrap choices
-// //         choicesContainer.append(choiceBtn);
-// //     }
+    }
+    displayQuestionEl.append(choicesContainer);
+
+   
+
+}
+
+//*****Logic to check for right answer */
+
+function checkAnswer(event) {
+
+    //***Logic to CHECK FOR RIGHT ANSWER */
+    var responseText = event.target.textContent;
+    console.log(responseText); 
 
 
-// //     // append div element to the question container element 
-// //     displayQuestionEl.append(choicesContainer);
-// // }
+    //increase index by 1 
+    index ++
+    if (responseText === questions[index].answer){
+        console.log("Correct");
+    } else {
+        console.log("Incorrect");
+    }
 
+    nextQuestion();
 
-// // // function to check answer and display to following question
-// // function checkAnswer(event) {
-
-// // }
-
-
-// // // function that shows the question and starts the timer
-// // function checkAnswer(event) {
-
-// //     //*****Logic to check for right answer */
-// // }
-
+}
 
 // // // add event listener to start quiz 
-// // startBtn.addEventListener("click", startQuiz)
-
+startBtn.addEventListener("click", startQuiz)
 // call function to show opening page
 openingPage(); 
